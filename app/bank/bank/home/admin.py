93235cfb2b583f5c125dbecf6fa66ce52fbdb0a0
LENOVO@DESKTOP <pranitbhoy68@.gmail.com>
2023-04-26 18:11:12 +0530

    remove bank

--- app/bank/bank/home/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from.models import USER_details
-class productAdmin(admin.ModelAdmin):
-    list_display = ['firstname','lastname','email_id']
-
-
-admin.site.register(USER_details,productAdmin)
--- app/bank/bank/home/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class USER_details(models.Model):
-    firstname=models.CharField(max_length=30)
-    lastname= models.CharField(max_length=20)
-    email_id= models.CharField(max_length=20)
-
-
